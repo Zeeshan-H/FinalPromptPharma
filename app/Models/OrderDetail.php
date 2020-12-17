c7c00b45067c1a61e991972eed5ae73d8567f474
Zeeshan-H <zeeshanh184@gmail.com>
2020-12-17 15:36:53 +0500

    Uo

---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    use HasFactory;
+
+    protected $table = 'order_details';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = [
+
+        'order_id',
+        'quantity',
+        'user_id',
+        'name',
+        'pharmaname',
+        'product_id',
+        'unit_price',
+    ];
+}
